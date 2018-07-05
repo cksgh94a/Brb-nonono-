27cb3986cb7b1bab918b528a160ff5e7775c7b18
cksgh94a <cksgh94a@gmail.com>
2018-07-05 19:01:33 +0900

    1

--- EclipseProj/BORABOT/src/initializing.java
+++ EclipseProj/BORABOT/src/initializing.java
@@ -1,63 +1,31 @@
-
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.time.LocalDate;
-//import java.time.LocalTime;
-//
-//import com.google.gson.Gson;
-//import com.google.gson.JsonObject;
-//import com.google.gson.JsonParser;
-//
-//import java.util.*;
-//import java.io.BufferedWriter;
-//import java.io.File;
-//import java.io.FileWriter;
+import java.util.Date;
 
 class initializing {
-
-	// websocket 통신으로 받아오는 부분
-	// 동시에 db저장
-	// NowTrading에 쓰이는 부분
-	// nowTrading or Trade?
-	private static String exchange = "bithumb";
-	private static String coin_target = "btc";
-	private static String coin_base = "krw";
-	private static String _ID = "dirtyrobot00";
-	private static double priceAmount = 7500000;
-	private static String startDate = "180606";
-	private static String endDate = "180704";
-	private static String botName = "패턴아 맞아라";
-	private static String strategyName = "patternNaked";
+	private String exchange;
+	private String coin_target;
+	private String coin_base = "krw";
+	private String _ID;
+	private double priceAmount;
+	private Date startDate;
+	private Date endDate;
+	private String botName;
+	private String strategyName;
 	
 	public initializing(TradingElement t) {
 		this.exchange = t.getExchange();
 		this.coin_target = t.getCoin();
 		this._ID = t.getId();
 		this.priceAmount = t.getPrice();
+		this.startDate = t.getStartDate();
+		this.endDate = t.getEndDate();
 		this.botName = t.getId()+t.getName();
 		this.strategyName = t.getStrategy();		
 	}
 
-	// db에서 받아옴
-	// SELECT APIKEY, Secret_KEY FROM Customer, APIKEY WHERE Customer._id =
-	// APIKEY._id AND exchange = {exchange}
-	//private static String API_KEY = "485f69323ae844f99f2ef3ae81692a1e";
-	//private static String Secret_KEY = "3289895f108b435e8a4633df2b5cdf61";
-	private static String API_KEY="";
-	private static String Secret_KEY="";
+	private String API_KEY="";
+	private String Secret_KEY="";
 	
 	public void main() {
-		// TODO Auto-generated method stub
-		// 함수내부로
-		// 추후에 tradingBot에다가 이 걸 넘겨줘도 됨
-//		String APIK;
-//		String SecK;
-//		String selectSql = String.format("SELECT API_KEY, Secret_KEY FROM APIKEY WHERE _ID = \"%s\" and exchangeName = \"%s\" ;", _ID, exchange);
-//		DB.useDB(selectSql, "select");	// key select
-
 		String coin_ex = "";
 		String coin_crypto = coin_target + coin_base;
 		if (exchange.equals("bittrex")) {
@@ -79,70 +47,5 @@ class initializing {
 		else 
 			System.out.println("전략 선택 오류다!!!");
 	}
-	
-	/*
-	public static void SQL_Select() {
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null; // ResultSet 객체 선언
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("드라이버 로드 성공!");
-			conn = DriverManager.getConnection(url);
-			System.out.println("데이터베이스 접속 성공!");
-			stmt = conn.createStatement();
-			//String selectSql = String.format("INSERT INTO trade VALUES(\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", %s, \"%s\", \"%s\", \"%s\" ,\"%s\")", _ID, coin_target, priceAmount, "null", strategyName, 1, exchange, botName, startDate, endDate);
-			String selectSql = String.format("SELECT API_KEY, Secret_KEY FROM APIKEY WHERE _ID = \"%s\" and exchangeName = \"%s\" ;", _ID, exchange);
-			System.out.println(selectSql);
-			rs = stmt.executeQuery(selectSql);
-			
-			while(rs.next()) {  
-				
-				API_KEY = rs.getString(1);
-				Secret_KEY = rs.getString(2);
-			}
-
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException se) {
-			se.printStackTrace();
-		} finally {
-			if (conn != null)
-				try {
-					conn.close();
-				} catch (SQLException e) {
-				}
-		}
-	}
-	
-	public static void SQL_Insert() {
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null; // ResultSet 객체 선언
-		
-		try {
-
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("드라이버 로드 성공!");
-			conn = DriverManager.getConnection(url);
-			System.out.println("데이터베이스 접속 성공!");
-			stmt = conn.createStatement();
-			String selectSql = String.format("INSERT INTO trade VALUES(\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", %s, \"%s\", \"%s\", \"%s\" ,\"%s\")", _ID, coin_target, priceAmount, "null", strategyName, 1, exchange, botName, startDate, endDate);
-			System.out.println(selectSql);
-			stmt.executeUpdate(selectSql);
-
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException se) {
-			se.printStackTrace();
-		} finally {
-			if (conn != null)
-				try {
-					conn.close();
-				} catch (SQLException e) {
-				}
-		}
-	}*/
 
 }
--- EclipseProj/BORABOT/src/tradingBot.java
+++ EclipseProj/BORABOT/src/tradingBot.java
@@ -20,44 +20,34 @@ class tradingBot {
 
 	Gson gson = new Gson();
 
-	String botName;
-	String _ID;
-	String start;
-	String end;
-	String exchange;
-	String coin_exchange;
-	String coin_crypto;
+	private String botName;
+	private String _ID;
+	private Date start;
+	private Date end;
+	private String exchange;
+	private String coin_exchange;
+	private String coin_crypto;
 	private String Algoset;
 	private String API_KEY;
 	private String Sec_KEY;
 	private double priceAmount; // 처음 시작 하는 돈
 	private double coinmany = 0;
+	private double testStartAsset;
 
 	// Algoset에서 뽑아내야함
-	int _period_day = 20;
-	int _mul = 2;
-	int _interval = 60;
-	int _corrInterval = 60; // 패턴인식하는데 1분봉을 사용, 즉 1분마다 서치
-	int _totalLength = 1440; // 하루치 데이터의 패턴을 비교
-	int _intervalNumber = 15; // 15분의 패턴을 본다는 뜻
+	private int _period_day = 20;
+	private int _mul = 2;
+	private int _interval = 60;
+	private int _corrInterval = 60; // 패턴인식하는데 1분봉을 사용, 즉 1분마다 서치
+	private int _totalLength = 1440; // 하루치 데이터의 패턴을 비교
+	private int _intervalNumber = 15; // 15분의 패턴을 본다는 뜻
 
 	// test 초기값
 	int testNum = 0; // 비트코인 0개
-	double testStartAsset = 1000000000; // 10억
 
-	// DB
-	private Connection conn = null;
-	private Statement stmt = null;
-	private ResultSet rs = null; // ResultSet 객체 선언
-	private String url = "jdbc:mysql://127.0.0.1/practice_db?user=root&password=01028798551&serverTimezone=UTC";
-	// private String url =
-	// "jdbc:mysql://127.0.0.1/test_ljh?user=root&serverTimezone=UTC";
-	
-	//File
-	FileWriter writer;
-	BufferedWriter bufWriter;
-	
-	public tradingBot(double priceAmount, String _ID, String start, String end, String exchange, String coin_crypto,
+	DB useDB = new DB();	// DB 사용 위한 DB 객체
+
+	public tradingBot(double priceAmount, String _ID, Date start, Date end, String exchange, String coin_crypto,
 			String coin_exchange, String Algoset, String API_KEY, String Sec_KEY, String botName) {
 		this.priceAmount = priceAmount;
 		this.testStartAsset = priceAmount;
@@ -72,22 +62,11 @@ class tradingBot {
 		this.Sec_KEY = Sec_KEY;
 		this.botName = botName;
 		this.priceAmount = priceAmount;
-		try {
-			writer = new FileWriter("C:\\Users\\Lee Jun-hyeong\\Desktop\\patternlog.txt");
-			bufWriter = new BufferedWriter(writer);
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
+
 		try {
 
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("드라이버 로드 성공!");
-			conn = DriverManager.getConnection(url);
-			System.out.println("데이터베이스 접속 성공!");
-			stmt = conn.createStatement();
-			String selectSql = "SELECT * FROM transaction_log;";
-
-			rs = stmt.executeQuery(selectSql); // rs에 executeQuery의 실행결과를 삽입
+			String selectSql = "SELECT * FROM trans_log;";
+			ResultSet rs = useDB.Query(selectSql, "select");
 
 			while (rs.next()) // next()에 대한 설명은 본문에
 			{
@@ -95,13 +74,9 @@ class tradingBot {
 						rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4) + "\t" + rs.getString(5));
 			}
 
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
 		} catch (SQLException se) {
 			se.printStackTrace();
-		} finally {
-			// if(conn!=null) try {conn.close();} catch (SQLException e) {}
-		}
+		} 
 	}
 
 	public double corrPatternAnalysis(Cryptowatch crypt, int interval, int totalLength, int intervalNumber) {
@@ -112,19 +87,6 @@ class tradingBot {
 		for (int k = 0; k < intervalNumber; k++) {
 			System.out.print(currentData[k] + " ");
 		}
-		
-		
-//		try {
-//			bufWriter = new BufferedWriter(writer);
-//			bufWriter.write("현재부터 15분 전까지 데이터 : ".toString());
-//			for (int k = 0; k < intervalNumber; k++) {
-//				bufWriter.write(currentData[k] + " ");	
-//			}
-//			bufWriter.newLine();bufWriter.flush();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		
 
 		double currentSum = 0;
 		for (double i : currentData) {
@@ -158,29 +120,14 @@ class tradingBot {
 				max = cosineDistance;
 				idx = i;
 			}
-			// System.out.println("max :" + max + " idx :" + idx);
 		}
 
-		// System.out.println("max :" + max + " idx :" + idx);
 
 		System.out.print("\n가장 일치하는 데이터 : ");
 		for (int k = idx; k < idx + intervalNumber; k++) {
 			System.out.print(dayData[k] + " ");
 		}
-		
-		/*
-		try {
-			bufWriter = new BufferedWriter(writer);
-			bufWriter.write("가장 일치하는 데이터 : ");
-			for (int k = idx; k < idx + intervalNumber; k++) {
-				bufWriter.write(dayData[k] + " ");
-			}
-			bufWriter.newLine();
-			bufWriter.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		*/
+
 
 		// 추세측정 idx+intervalNumber ~ 15개
 		idx += 15;
@@ -191,27 +138,13 @@ class tradingBot {
 		}
 		double corr = new PearsonsCorrelation().correlation(y, x);
 
-		
+
 		System.out.print("\n추후 데이터 : ");
 		for (int k = 0; k < intervalNumber; k++) {
 			System.out.print(y[k] + " ");
 		}
 		System.out.println();
-		
-		/*
-		try {
-			bufWriter = new BufferedWriter(writer);
-			bufWriter.write("추후 데이터 : ");
-			for (int k = 0; k < intervalNumber; k++) {
-				bufWriter.write(y[k] + " ");
-			}
-			bufWriter.newLine();
-			bufWriter.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		*/
-		
+
 		return corr;
 	}
 
@@ -221,12 +154,8 @@ class tradingBot {
 		Bittrex brx = new Bittrex(API_KEY, Sec_KEY, 30, 1); // 이부분은 차차 개선 -> 여러가지 거래소도 동일하게 추상화 필요
 		Cryptowatch crypt = new Cryptowatch(10, 1); // 1회성 콜 -> 이대로 사용해도 괜찮음, 거래를 하지 않기 때문에 괜춘
 
-//		try {
-//        // 파일 객체 생성
-//        File file = new File("/usr/local/server/apache-tomcat-8.0.52/webapps/"+this.botName+".txt");
-//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
-        
-		while (TradeMain.map.get(botName)) {
+		boolean run = true;
+		while (run) {
 			double[] currentData = getHistoryArray(crypt, _corrInterval, _intervalNumber, _intervalNumber);
 			double corr = corrPatternAnalysis(crypt, _corrInterval, _totalLength, _intervalNumber);
 			System.out.println();
@@ -237,26 +166,26 @@ class tradingBot {
 			} else {
 				doNothing();
 			}        
-//			
-//            if(file.isFile() && file.canWrite()){
-//                Date d = new Date();            
-//                String time = d.toString();
-//                bufferedWriter.write(this.botName + " " + time + SalesBot.map + "\n" + new Date());	// 쓰기
-//                bufferedWriter.newLine();	// 개행문자쓰기  
-//            }
-	        
 
 			try {
-				Thread.sleep(6000*10+50);
+				Thread.sleep(60*10+50);
 			} catch (Exception e) {
 				System.out.println(e.toString());
 			}
 			System.out.println();
+			
+			String selectSql = String.format(
+					"select on_going from trade where user_id = \'%s\' and bot_name = \'%s\'", _ID, botName);
+
+			ResultSet rs = useDB.Query(selectSql, "select");
+			try {
+				while(rs.next()) {  
+					run = rs.getBoolean("on_going");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();			
+			}
 		}
-//		bufferedWriter.close();	// 로그는 임시로 마지막에 한번에 생성하게 해놈
-//        
-//        } catch(Exception e) {        	
-//        }
 	}
 
 	public void trendFollowing(int less_ave, int more_ave) {
@@ -309,95 +238,75 @@ class tradingBot {
 		// 값을 받고 비교 -> 로직
 		// B S W 리턴 -> 실행		
 		// queue 새로세팅
-		
 
-//        try {
-//            // 파일 객체 생성
-//            File file = new File("/usr/local/server/apache-tomcat-8.0.52/webapps/"+this.botName+".txt");
-//            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
 
-    		while (TradeMain.map.get(botName)) {
 
-//    			try {
-//    				bufWriter = new BufferedWriter(new FileWriter("C:\\Users\\Lee Jun-hyeong\\Desktop\\patternlog.txt"));
-//    			} catch (IOException e1) {
-//    				// TODO Auto-generated catch block
-//    				e1.printStackTrace();
-//    			}
-    			
-    			double[] bollingerHL = getBollinger(crypt, history_queue, _mul);
-    			// ------------------
-    			try {
-    				Thread.sleep(1000 * _interval);
-    			} catch (Exception e) {
-    				System.out.println(e.toString());
-    			}
-    			// -------------------
-    			double currentLast = getCurrentPrice(crypt, coin_crypto);
-    			System.out.println(" --> 현재가 : " + currentLast);
-    			// -----------------
-    			// 매수타이밍
-    			if (currentLast < bollingerHL[1]) {
-    				// BUY
-    				// 얼마나 살건지 알고리즘 셋팅에 따라
-    				System.out.println("잠정적 매수 타이밍");
-    				double corr = corrPatternAnalysis(crypt, _corrInterval, _totalLength, _intervalNumber);
-    				if (corr > 0.4) {
-    					System.out.print("Perason Correlation Coefficieint : " + corr+ " ");
-    					buyCoin(brx, 50, currentLast);
-    				} else if (corr < -0.5) {
-    					System.out.print("Perason Correlation Coefficieint : " + corr+ " ");
-    					sellCoin(brx, 40, currentLast);
-    				
-    				} else {
-    					doNothing();
-    				}
-    			}
-    			// 매도타이밍
-    			else if (currentLast > bollingerHL[0]) {
-    				// SELL
-    				// 얼마나 팔건지 알고리즘 셋팅에 따라
-    				System.out.println("잠정적 매도 타이밍");
-    				double corr = corrPatternAnalysis(crypt, _corrInterval, _totalLength, _intervalNumber);
-    				if (corr > 0.3) {
-    					System.out.print("Perason Correlation Coefficieint : " + corr+ " ");
-    					buyCoin(brx, 20, currentLast);
-    				} else if (corr < -0.5) {
-    					System.out.print("Perason Correlation Coefficieint : " + corr+ " ");
-    					sellCoin(brx, 80, currentLast);
-    				} else {
-    					doNothing();
-    				}
-    			}
-    			// 대기타이밍
-    			else {
-    				// wait
-    				System.out.println("잠정적 대기 타이밍");
-    				double corr = corrPatternAnalysis(crypt, _corrInterval, _totalLength, _intervalNumber);
-    				if (corr > 0.75) {
-    					//4개구매
-    					System.out.print("Perason Correlation Coefficieint : " + corr+ " ");
-    					buyCoin(brx, 30, currentLast);
-    				} else {
-    					System.out.print("Perason Correlation Coefficieint : " + corr+ " ");
-    					doNothing();
-    				}
-    			}
-    			// -----------------
-    			history_queue.remove();
-    			history_queue.add(currentLast);
-    			System.out.println("돈으로 환산한 총 현재 재산 : " + (int)(currentLast * testNum + testStartAsset) + "KRW");
-    			System.out.println();
-//                if(file.isFile() && file.canWrite()){
-//                    bufferedWriter.write(this.botName + " 보유자산 : " + (int)(currentLast * testNum + testStartAsset) + "KRW\n" + new Date());	// 쓰기
-//                    bufferedWriter.newLine();	// 개행문자쓰기  
-//                }
-    		}
-//    		bufferedWriter.close();	// 로그는 임시로 마지막에 한번에 생성하게 해놈
-            
-//        } catch(Exception e) {        	
-//        }
+		while (TradeMain.map.get(botName)) {
 
+			double[] bollingerHL = getBollinger(crypt, history_queue, _mul);
+			// ------------------
+			try {
+				Thread.sleep(1000 * _interval);
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+			// -------------------
+			double currentLast = getCurrentPrice(crypt, coin_crypto);
+			System.out.println(" --> 현재가 : " + currentLast);
+			// -----------------
+			// 매수타이밍
+			if (currentLast < bollingerHL[1]) {
+				// BUY
+				// 얼마나 살건지 알고리즘 셋팅에 따라
+				System.out.println("잠정적 매수 타이밍");
+				double corr = corrPatternAnalysis(crypt, _corrInterval, _totalLength, _intervalNumber);
+				if (corr > 0.4) {
+					System.out.print("Perason Correlation Coefficieint : " + corr+ " ");
+					buyCoin(brx, 50, currentLast);
+				} else if (corr < -0.5) {
+					System.out.print("Perason Correlation Coefficieint : " + corr+ " ");
+					sellCoin(brx, 40, currentLast);
+
+				} else {
+					doNothing();
+				}
+			}
+			// 매도타이밍
+			else if (currentLast > bollingerHL[0]) {
+				// SELL
+				// 얼마나 팔건지 알고리즘 셋팅에 따라
+				System.out.println("잠정적 매도 타이밍");
+				double corr = corrPatternAnalysis(crypt, _corrInterval, _totalLength, _intervalNumber);
+				if (corr > 0.3) {
+					System.out.print("Perason Correlation Coefficieint : " + corr+ " ");
+					buyCoin(brx, 20, currentLast);
+				} else if (corr < -0.5) {
+					System.out.print("Perason Correlation Coefficieint : " + corr+ " ");
+					sellCoin(brx, 80, currentLast);
+				} else {
+					doNothing();
+				}
+			}
+			// 대기타이밍
+			else {
+				// wait
+				System.out.println("잠정적 대기 타이밍");
+				double corr = corrPatternAnalysis(crypt, _corrInterval, _totalLength, _intervalNumber);
+				if (corr > 0.75) {
+					//4개구매
+					System.out.print("Perason Correlation Coefficieint : " + corr+ " ");
+					buyCoin(brx, 30, currentLast);
+				} else {
+					System.out.print("Perason Correlation Coefficieint : " + corr+ " ");
+					doNothing();
+				}
+			}
+			// -----------------
+			history_queue.remove();
+			history_queue.add(currentLast);
+			System.out.println("돈으로 환산한 총 현재 재산 : " + (int)(currentLast * testNum + testStartAsset) + "KRW");
+			System.out.println();
+		}
 	}
 
 	public void Bollingertrade() {
@@ -444,7 +353,7 @@ class tradingBot {
 			// -----------------
 			history_queue.remove();
 			history_queue.add(currentLast);
-        	
+
 			System.out.println();
 		}
 	}
@@ -474,25 +383,25 @@ class tradingBot {
 			testStartAsset -= price * how;
 			System.out.print("구매한 코인의 수 : "+how+", 구매한 코인의 개당 가격 : " + price + " --> ");
 		}
-		
+
 		Cryptowatch crypt = new Cryptowatch(10, 1);
 		double currentLast = getCurrentPrice(crypt, coin_crypto);
 
-    	for(int i = TradeMain.nowTrading.size() - 1; i >= 0; i--) {
-    		
-    		if((TradeMain.nowTrading.get(i).getId()+TradeMain.nowTrading.get(i).getName()).equals(botName)) {
-    			TradeMain.nowTrading.get(i).setProfit(String.format("%.2f", ((currentLast * (double)testNum + testStartAsset)/priceAmount*100.0)));
-    			double asd = ((currentLast * (double)testNum + testStartAsset)/priceAmount*100.0);
-    			System.out.println();
-    			System.out.println(String.format("%.2f", asd));
-    			System.out.println();
-    		}
-		} 
-    	
+//		for(int i = TradeMain.nowTrading.size() - 1; i >= 0; i--) {
+//
+//			if((TradeMain.nowTrading.get(i).getId()+TradeMain.nowTrading.get(i).getName()).equals(botName)) {
+//				TradeMain.nowTrading.get(i).setProfit(String.format("%.2f", ((currentLast * (double)testNum + testStartAsset)/priceAmount*100.0)));
+//				double asd = ((currentLast * (double)testNum + testStartAsset)/priceAmount*100.0);
+//				System.out.println();
+//				System.out.println(String.format("%.2f", asd));
+//				System.out.println();
+//			}
+//		} 
+
 		// 디비 쿼리 날려주기
 		// transaction log로 날려    	
 		System.out.println("현재 코인 보유 수 : " + testNum + " / 현재 돈 : " + testStartAsset + "KRW");
-		ExecSQL_Insert(_ID, exchange, coin_crypto, 1, amount, price);
+//		ExecSQL_Insert(_ID, exchange, coin_crypto, 1, amount, price);
 	}
 
 	public void sellCoin(Bittrex brx, double amount, double price) {
@@ -513,23 +422,23 @@ class tradingBot {
 			testNum -= testNum;
 			System.out.print("판매한 코인의 수 : "+temp+", 판매한 코인의 개당 가격 : " + price + " --> ");
 		}
-		
+
 		Cryptowatch crypt = new Cryptowatch(10, 1);
 		double currentLast = getCurrentPrice(crypt, coin_crypto);
 
-    	for(int i = TradeMain.nowTrading.size() - 1; i >= 0; i--) {
-    		
-    		if((TradeMain.nowTrading.get(i).getId()+TradeMain.nowTrading.get(i).getName()).equals(botName)) {
-    			TradeMain.nowTrading.get(i).setProfit(String.format("%.2f", ((currentLast * (double)testNum + testStartAsset)/priceAmount*100.0)));
-    			double asd = ((currentLast * (double)testNum + testStartAsset)/priceAmount*100.0);
-    			System.out.println(String.format("%.2f", asd));
-    		}
-		} 
+//		for(int i = TradeMain.nowTrading.size() - 1; i >= 0; i--) {
+//
+//			if((TradeMain.nowTrading.get(i).getId()+TradeMain.nowTrading.get(i).getName()).equals(botName)) {
+//				TradeMain.nowTrading.get(i).setProfit(String.format("%.2f", ((currentLast * (double)testNum + testStartAsset)/priceAmount*100.0)));
+//				double asd = ((currentLast * (double)testNum + testStartAsset)/priceAmount*100.0);
+//				System.out.println(String.format("%.2f", asd));
+//			}
+//		} 
 		// 디비 쿼리 날려주기
 		// transaction log로 날려줌
 		System.out.println("현재 코인 보유 수 : " + testNum + " / 현재 돈 : " + testStartAsset + "KRW");
-		ExecSQL_Insert(_ID, exchange, coin_crypto, 2, amount, price);
-		
+//		ExecSQL_Insert(_ID, exchange, coin_crypto, 2, amount, price);
+
 	}
 
 	public void doNothing() {
@@ -537,21 +446,21 @@ class tradingBot {
 
 		System.out.println("현재 코인 보유 수 : " + testNum + " / 현재 돈 : " + testStartAsset + "KRW");
 		// 디비 쿼리 날려주기
-		ExecSQL_Insert(_ID, exchange, coin_crypto, 3, 0, 0);
-		
+//		ExecSQL_Insert(_ID, exchange, coin_crypto, 3, 0, 0);
+
 		Cryptowatch crypt = new Cryptowatch(10, 1);
 		double currentLast = getCurrentPrice(crypt, coin_crypto);
 
-    	for(int i = TradeMain.nowTrading.size() - 1; i >= 0; i--) {
-    		
-    		if((TradeMain.nowTrading.get(i).getId()+TradeMain.nowTrading.get(i).getName()).equals(botName)) {
-    			TradeMain.nowTrading.get(i).setProfit(String.format("%.2f", ((currentLast * (double)testNum + testStartAsset)/priceAmount*100.0)));
-    			double asd = ((currentLast * (double)testNum + testStartAsset)/priceAmount*100.0);
-    			System.out.println();
-    			System.out.println(String.format("%.2f", asd));
-    			System.out.println();
-    		}
-		} 
+//		for(int i = TradeMain.nowTrading.size() - 1; i >= 0; i--) {
+//
+//			if((TradeMain.nowTrading.get(i).getId()+TradeMain.nowTrading.get(i).getName()).equals(botName)) {
+//				TradeMain.nowTrading.get(i).setProfit(String.format("%.2f", ((currentLast * (double)testNum + testStartAsset)/priceAmount*100.0)));
+//				double asd = ((currentLast * (double)testNum + testStartAsset)/priceAmount*100.0);
+//				System.out.println();
+//				System.out.println(String.format("%.2f", asd));
+//				System.out.println();
+//			}
+//		} 
 	}
 
 	// 가격 히스토리를 보여줌 -> ohlc 중 뭘 사용할건지 추가 / interval과 갯수 파라미터
@@ -650,24 +559,24 @@ class tradingBot {
 		return currentLast;
 	}
 
-	public void ExecSQL_Insert(String _ID, String exchange_name, String coin_name, int sale, double amount,
-			double currentPrice) {
-
-		// sale -> 1: BUY, 2: SELL, 3: WAIT
-		String currentTime = LocalTime.now().toString();
-		String val = "(\"" + botName + "\"," + "\"" + _ID + "\",\"" + currentTime + "\", " + sale + "," + "\""
-				+ exchange_name + "\",\"" + coin_name + "\");";
-		String selectSql = "INSERT INTO transaction_log VALUES" + val;
-
-		try {
-			stmt = conn.createStatement();
-			stmt.executeUpdate(selectSql);
-			//System.out.println("sql문 삽입 성공 : " + selectSql);
-		}
-
-		catch (Exception e) {
-			System.out.println(e.toString());
-		}
-	}
+//	public void ExecSQL_Insert(String _ID, String exchange_name, String coin_name, int sale, double amount,
+//			double currentPrice) {
+//
+//		// sale -> 1: BUY, 2: SELL, 3: WAIT
+//		String currentTime = LocalTime.now().toString();
+//		String val = "(\"" + botName + "\"," + "\"" + _ID + "\",\"" + currentTime + "\", " + sale + "," + "\""
+//				+ exchange_name + "\",\"" + coin_name + "\");";
+//		String selectSql = "INSERT INTO transaction_log VALUES" + val;
+//
+//		try {
+//			stmt = conn.createStatement();
+//			stmt.executeUpdate(selectSql);
+//			//System.out.println("sql문 삽입 성공 : " + selectSql);
+//		}
+//
+//		catch (Exception e) {
+//			System.out.println(e.toString());
+//		}
+//	}
 
 }
