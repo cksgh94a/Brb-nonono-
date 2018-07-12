3fd603921783c143473cacf2fc1d4141c1f03835
cksgh94a <cksgh94a@gmail.com>
2018-07-12 16:59:04 +0900

    1

--- VSCode/hot/src/App.js
+++ VSCode/hot/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ChartSelect from './components/main/ChartSelect';
+import Sales from './components/main/Sales';
+import NowTrading from './components/main/NowTrading';
+import WalletInfo from './components/main/WalletInfo';
+import Login from './components//initial/Login';
 
 class App extends Component {
+
   render() {
+
     return (
+      <div>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get startedasdfsadf, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="wrapper">
+        <div className="one"><NowTrading/></div>
+        <div className="three"><ChartSelect/></div>
+        <div className="four"><WalletInfo/></div>
+        <div className="five"><Sales/></div>
+      </div>
+      </div>
+      <Login/>      
       </div>
     );
   }
 }
 
 export default App;
+
