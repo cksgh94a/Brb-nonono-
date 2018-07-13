98c60e7cc0b2ca1883a7446c904bb77c9c5d8edb
cksgh94a <cksgh94a@gmail.com>
2018-07-13 16:24:26 +0900

    1

--- VSCode/borabot/src/header/Header.js
+++ VSCode/borabot/src/header/Header.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Main from '../main/Main';
 
 class Header extends Component {
 
   render() {
     return (
         <div>
-          <Link to="/main">Borabot</Link>
+          <Link to='../main/Main.js'>Borabot</Link>
+          {/* <Link to="/main">Borabot</Link>
           <Link to="/board">Board</Link>
           <Link to="/backtesting">BackTesting</Link>
           <Link to="/profile">Profile</Link>
-          <Link to="/">Init</Link>
+          <Link to="/">Init</Link> */}
         </div>
     );
   }
